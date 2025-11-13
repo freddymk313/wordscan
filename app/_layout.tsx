5d40100ce5a8a095ecc5cddb3121281f454da8ee
Freddymk <freddymk@MacBook-Air-de-INT.local>
2025-11-13 12:10:43 -0800

    first commit

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,13 +1,15 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { Stack } from 'expo-router';
-import { StatusBar } from 'expo-status-bar';
-import 'react-native-reanimated';
+import {
+  DarkTheme,
+  DefaultTheme,
+  ThemeProvider,
+} from "@react-navigation/native";
+import { Stack } from "expo-router";
+import { StatusBar } from "expo-status-bar";
+// import 'react-native-reanimated';
+import "../global.css";
 
-import { useColorScheme } from '@/hooks/use-color-scheme';
-
-export const unstable_settings = {
-  anchor: '(tabs)',
-};
+import { useColorScheme } from "@/hooks/use-color-scheme";
+import { Text, View } from "react-native";
 
 export default function RootLayout() {
   const colorScheme = useColorScheme();
@@ -15,9 +17,10 @@ export default function RootLayout() {
   return (
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
       <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="modal" options={{ presentation: 'modal', title: 'Modal' }} />
+        <Stack.Screen name="index" options={{ headerShown: false }} />
+        {/* <Stack.Screen name="register" options={{ headerShown: false }} /> */}
       </Stack>
+
       <StatusBar style="auto" />
     </ThemeProvider>
   );
