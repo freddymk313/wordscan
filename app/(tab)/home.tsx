df83bbafb5abedc8ffc8b2a0450e027c8e53b9dd
Freddymk <freddymk@MacBook-Air-de-INT.local>
2025-11-24 11:37:01 -0800

    design icon added

--- app/(tab)/home.tsx
+++ app/(tab)/home.tsx
@@ -1,12 +1,25 @@
 import { View, Text } from "react-native";
 import React from "react";
+import AntDesign from '@expo/vector-icons/AntDesign';
+import AntDesign from '@expo/vector-icons/AntDesign';
 import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function home() {
   return (
-    <SafeAreaView className="bg-white">
-      <View>
-        <Text>home</Text>
+    <SafeAreaView className="flex-1 bg-white">
+      <View className="px-6 py-6">
+        <View className="flex-row items-center justify-between">
+          <Text className="text-3xl font-bold">Wordscan</Text>
+          <AntDesign name="moon" size={22} color="black" />
+        </View>
+
+        <View>
+          <View>
+
+          </View>
+
+          <View></View>
+        </View>
       </View>
     </SafeAreaView>
   );
